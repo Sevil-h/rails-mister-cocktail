bddb1a692b8346ffc96e6e67d6e9839eab3208f7
sevil hatipoglu <sevilhatipogluu93@gmail.com>
2021-03-01 20:20:05 +0000

    Heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,7 @@ require 'open-uri'
 
 url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
 file = open(url).read
-ingredients = JSON.parse(file)['drinks'].sort!
+ingredients = JSON.parse(file)['drinks']
 ing = ingredients.each do |ingredient|
   Ingredient.create!(name: ingredient['strIngredient1'])
 end
