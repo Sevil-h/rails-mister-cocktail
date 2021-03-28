860fe0444f3df333c01fe29d92a39c9a665190b4
sevil hatipoglu <sevilhatipogluu93@gmail.com>
2021-03-28 18:26:03 +0300

    Added destroy method in seed

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -1,10 +1,9 @@
 class Dose < ApplicationRecord
 
   validates :description, presence: true
-  # validates :cocktail_id, uniqueness: true
-  # validates :ingredient_id, uniqueness: true
+  validates :cocktail_id, uniqueness: true
+  validates :ingredient_id, uniqueness: true
   validates :cocktail, uniqueness: { scope: :ingredient }
-
   belongs_to :cocktail
   belongs_to :ingredient
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,11 @@
 require 'json'
 require 'open-uri'
 
+Cocktail.destroy_all
+Dose.destroy_all
+Ingredient.destroy_all
+Review.destroy_all
+
 url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
 file = open(url).read
 ingredients = JSON.parse(file)['drinks']
